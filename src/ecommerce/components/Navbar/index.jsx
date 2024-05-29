f8ab2b05b5998957d34f22945faced08dd51b5c8
Jose Gamarra <joselogam@gmail.com>
2024-05-29 19:30:50 -0400

    Feat: Start creating Navbar

---
+++ src/ecommerce/components/Navbar/index.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom"
+
+const Navbar  = () => {
+    <nav>
+        <ul>
+            <li>
+                <NavLinkx
+                    to = '/'>
+                    Shopping
+                </NavLinkx>
+            </li>
+        </ul>
+    </nav>
+}
