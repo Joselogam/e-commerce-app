528b41fe93b4abb4f6051ce23d613f09e8fae940
Jose Gamarra <joselogam@gmail.com>
2024-05-30 13:31:54 -0400

    Feat: Begin to create same styles for webpages

--- src/ecommerce/components/Navbar/index.jsx
+++ src/ecommerce/components/Navbar/index.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from "react-router-dom" 
 
 const Navbar  = () => {
+    const activeStyle = "underline underline-offset-4"
+    
     return (
             <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
                 <ul className="flex items-center gap-3">
                     <li className="font-semibold text-lg">
-                        <NavLink
-                            to = '/'>
+                        <NavLink to = '/'>
                             Shopping
                         </NavLink>
                     </li>
@@ -18,54 +19,62 @@ const Navbar  = () => {
                     </li>
                     <li>
                         <NavLink
-                            to = '/clothes'>
+                            to = '/clothes'
+                            className={({isActive}) => isActive?activeStyle:undefined}>
                             Clothes
                         </NavLink>
                     </li>
                     <li>
                         <NavLink
-                            to = '/electronics'>
+                            to = '/electronics'
+                            className={({isActive}) => isActive?activeStyle:undefined}>
                         Electronics
                         </NavLink>
                     </li>
                     <li>
                         <NavLink
-                            to = '/furniture'>
+                            to = '/furniture'
+                            className={({isActive}) => isActive?activeStyle:undefined}>
                             Furniture
                         </NavLink>
                     </li>
                     <li>
                         <NavLink
-                            to = '/toys'>
+                            to = '/toys'
+                            className={({isActive}) => isActive?activeStyle:undefined}>
                             Toys
                         </NavLink>
                     </li>
                     <li>
                         <NavLink
-                            to = '/others'>
+                            to = '/others'
+                            className={({isActive}) => isActive?activeStyle:undefined}>
                             Others
                         </NavLink>
                     </li>
                 </ul>
                 <ul className="flex items-center gap-3">
-                    <li>
+                    <li className="text-black/60">
                         Jose@correoamigo.com
                     </li>
                     <li>
                         <NavLink
-                            to = '/my-orders'>
+                            to = '/my-orders'
+                            className={({isActive}) => isActive?activeStyle:undefined}>
                             My Orders
                         </NavLink>
                     </li>
                     <li>
                         <NavLink
-                            to = '/my-account'>
+                            to = '/my-account'
+                            className={({isActive}) => isActive?activeStyle:undefined}>
                             My Account 
                         </NavLink>
                     </li>
                     <li>
                         <NavLink
-                            to = '/sign-in'>
+                            to = '/sign-in'
+                            className={({isActive}) => isActive?activeStyle:undefined}>
                             Sign In
                         </NavLink>
                     </li>
