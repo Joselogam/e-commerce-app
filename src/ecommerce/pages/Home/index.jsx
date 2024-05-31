057374ed45d3edeab77de1469360096b2c20d685
Jose Gamarra <joselogam@gmail.com>
2024-05-30 23:14:52 -0400

    Feat: Halfway through making the cards with the API

--- src/ecommerce/pages/Home/index.jsx
+++ src/ecommerce/pages/Home/index.jsx
@@ -1,10 +1,23 @@
+import { useState, useEffect } from "react";
 import Layout from "../../components/Layout";
 import Card from "../../components/Card";
 
 function Home(){
+    const [items, setItems] = useState(null)    
+
+    useEffect(()=>{
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then(response=>response.json())
+        .then(data=>setItems(data))
+    },[])
     return( <Layout>
             Home
-            <Card/>
+            {
+                items?.map((item)=>{
+                    return (<Card key={item.id} data={item}/>)
+                } )
+            }
+            
             </Layout>
             )
         
